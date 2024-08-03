74993484656cc2b201e1bd548b93be871e7543bf
Matvey Bobrov <matveybobrov01@gmail.com>
2024-08-03 19:24:27 +0300

    feat: make background transparent

--- init.lua
+++ init.lua
@@ -796,6 +796,13 @@ require('lazy').setup({
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
     'folke/tokyonight.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
+    opts = {
+      transparent = true, -- Enable transparency
+      styles = {
+        sidebars = 'transparent', -- Make sidebars transparent
+        floats = 'transparent', -- Make floating windows transparent
+      },
+    },
     init = function()
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
